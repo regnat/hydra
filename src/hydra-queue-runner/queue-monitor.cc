regnat/hydra

commit bb12e9bce0fc5a11b5e804cbab9a43876576722a

Don't call queryPartialDerivationOutputMap on the dest store
Unless it's also the local store, this will likely fail because this
call requires having the derivation handy, which the dest store
generally won't have

src/hydra-queue-runner/queue-monitor.cc
@@ -464,12 +464,18 @@ Step::ptr State::createStep(ref<Store> destStore,
     auto outputHashes = staticOutputHashes(*localStore, *(step->drv));
     bool valid = true;
     std::map<DrvOutput, std::optional<StorePath>> missing;
-    for (auto & [outputName, maybeOutputPath] : destStore->queryPartialDerivationOutputMap(step->drvPath)) {
-      if (!(maybeOutputPath && destStore->isValidPath(*maybeOutputPath))) {
+    for (auto [outputName, outputHash] : outputHashes) {
+      if (! destStore->queryRealisation(DrvOutput{outputHash, outputName})) {
         valid = false;
-        missing.insert({{outputHashes.at(outputName), outputName}, maybeOutputPath});
+        missing.insert({{outputHash, outputName}, std::nullopt});
       }
     }
+    /* for (auto & [outputName, maybeOutputPath] : destStore->queryPartialDerivationOutputMap(step->drvPath)) { */
+    /*   if (!(maybeOutputPath && destStore->isValidPath(*maybeOutputPath))) { */
+    /*     valid = false; */
+    /*     missing.insert({{outputHashes.at(outputName), outputName}, maybeOutputPath}); */
+    /*   } */
+    /* } */
 
     /* Try to copy the missing paths from the local store or from
        substitutes. */

commit 01019b62309b529d5b88f27b759ff217cb9b9f7f

Correctly handle the non-ca case when building
Fix a bug introduced by bb12e9bce0fc5a11b5e804cbab9a43876576722a

src/hydra-queue-runner/queue-monitor.cc
@@ -464,18 +464,23 @@ Step::ptr State::createStep(ref<Store> destStore,
     auto outputHashes = staticOutputHashes(*localStore, *(step->drv));
     bool valid = true;
     std::map<DrvOutput, std::optional<StorePath>> missing;
-    for (auto [outputName, outputHash] : outputHashes) {
-      if (! destStore->queryRealisation(DrvOutput{outputHash, outputName})) {
-        valid = false;
-        missing.insert({{outputHash, outputName}, std::nullopt});
+    if (settings.isExperimentalFeatureEnabled("ca-derivations")) {
+      for (auto [outputName, outputHash] : outputHashes) {
+        if (! destStore->queryRealisation(DrvOutput{outputHash, outputName})) {
+          valid = false;
+          missing.insert({{outputHash, outputName}, std::nullopt});
+        }
+      }
+    } else {
+      for (auto & [outputName, maybeOutputPath] : step->drv->outputsAndOptPaths(*destStore)) {
+        // If we're not CA, all the output paths should be known
+        assert(maybeOutputPath.second);
+        if (!destStore->isValidPath(*maybeOutputPath.second)) {
+          valid = false;
+          missing.insert({{outputHashes.at(outputName), outputName}, maybeOutputPath.second});
+        }
       }
     }
-    /* for (auto & [outputName, maybeOutputPath] : destStore->queryPartialDerivationOutputMap(step->drvPath)) { */
-    /*   if (!(maybeOutputPath && destStore->isValidPath(*maybeOutputPath))) { */
-    /*     valid = false; */
-    /*     missing.insert({{outputHashes.at(outputName), outputName}, maybeOutputPath}); */
-    /*   } */
-    /* } */
 
     /* Try to copy the missing paths from the local store or from
        substitutes. */